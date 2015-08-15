2b7620176935c5736b8fe43d370faba746c4f88b
Alin Pandichi <alin.pandichi@gmail.com>
2015-08-15 20:27:59 +0300

    Database service with single CREATE sql statement

--- db-migrations-cordova/www/js/app.js
+++ db-migrations-cordova/www/js/app.js
@@ -3,9 +3,27 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.service('DatabaseService', function($ionicPlatform, $cordovaSQLite) {
+  this.migrate = function() {
+    var db = $cordovaSQLite.openDB({ name: "my.db", bgType: 1 });
+    var query = "CREATE TABLE IF NOT EXISTS person(id INTEGER PRIMARY KEY NOT NULL, firstname VARCHAR(100), lastname VARCHAR(100))";
+    console.log("will run query " + query);
+    $cordovaSQLite.execute(db, query, []).then(function(res) {
+      console.log("created");
+    }, function (err) {
+      console.error(JSON.stringify(err));
+    });
+  };
+  return this;
+})
+
+.run(function($ionicPlatform, DatabaseService) {
+  $ionicPlatform.ready(function() {
+    DatabaseService.migrate();
+  });
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +34,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
